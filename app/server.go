eb9724dd306c72370e213d4be911e5f2be389749
Vikalp Gandha <vikalpgandha9712@gmail.com>
2024-08-26 19:49:45 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -9,6 +9,21 @@ import (
 	"os"
 )
 
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	for {
+		resp := make([]byte, 50)
+		n, err := conn.Read(resp)
+		if err != nil && err != io.EOF {
+			fmt.Println("Error Reading from conn: ", err)
+			os.Exit(1)
+		}
+		if n > 0 {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
@@ -20,20 +35,12 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 	for {
-		resp := make([]byte, 50)
-		n, err := conn.Read(resp)
-		if err != nil && err != io.EOF {
-			fmt.Println("Error Reading from conn: ", err)
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		if n > 0 {
-			conn.Write([]byte("+PONG\r\n"))
-		}
+		go handleConn(conn)
 	}
 }
